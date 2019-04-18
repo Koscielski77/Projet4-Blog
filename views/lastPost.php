59ea0b3253ffb04ff2a4c5234dcca8331e9e90fa
Julien Morot <julien77176@outlook.com>
2019-04-18 13:24:04 +0100

    Modification mineur + commentaires des Posts

--- views/lastPost.php
+++ views/lastPost.php
@@ -1,16 +1,32 @@
 <?php $title = 'Dernier chapitre'; ?>
+
+<!-- On utilise ob_start() qui au lancement de la page démarre la temporisation de sortie. Tant qu'elle est enclenchée, aucune donnée, hormis les en-têtes, n'est envoyée au navigateur, mais temporairement mise en tampon. -->
+
 <?php ob_start(); ?>
 
+<!-- Je crée une div avec la class LastPost, c'est dans cette div que je placerais le titre
+Je recupère le titre grâce a $lastPost dans laquel je passe en paramètre title "donc le titre du dernier article" -->
+
 <div class="LastPost">
-<h4><?php echo $lastPost['title'] ?></h4>
+    <h4><?php echo $lastPost['title'] ?></h4>
 </div>
+
+<!-- J'ajoute un trait que j'ai fais grâce a une simple border pour apporter plus d'esthétisme -->
+
 <div class="trait"></div>
+
+<!-- Je m'occupe maintenant de retranscrire les données du dernier article -->
+
 <p>Publié le : <?php echo $lastPost['added_datetime_fr'] ?><br>
 <?php echo $lastPost['author'] ?></p>
 <p><?php echo html_entity_decode($lastPost['content']) ?> </p>
 <a href="?controller=PostController&action=showAction&id=<?= $lastPost['id'] ?>" title="Lire les commentaires">Lire les commentaires</a>
 <hr>
 
+<!-- On execute ob_get_clean qui lit le contenu courant du tampon de sortie puis l'efface -->
+
 <?php $content = ob_get_clean(); ?>
+
+<!-- On ajoute également la template -->
 
 <?php require('views/template.php'); ?>
--- views/listPosts.php
+++ views/listPosts.php
@@ -1,21 +1,36 @@
-<?php
-    $title = 'Mon blog';
-?>
+<?php $title = 'Liste des chapitres'; ?>
+
+<!-- On utilise ob_start() qui au lancement de la page démarre la temporisation de sortie. Tant qu'elle est enclenchée, aucune donnée, hormis les en-têtes, n'est envoyée au navigateur, mais temporairement mise en tampon. -->
 
 <?php ob_start(); ?>
 
-<h1>Liste des billets</h1>
 
 <?php
-    if (isset($_SESSION) && !empty($_SESSION))
-    {
-        echo "<p>Vous êtes connecté ! Bonjour " . $_SESSION['pseudo'] . " !</p>";
-    }
-    else {
-        echo "Bonjour visiteur !";
-    }
+while ($post = $posts->fetch())
+{ ?>
+
+        <!-- On crée une div ou seront inséré les différents articles 
+        On recupère le titre grâce a $post['title'], on utilise html_entity_decode qui convertit les entités HTML à leurs caractères correspondant
+        On ajoute ensuite un trait qui rend la page plus esthétique 
+        On ajoute ensuite la date de publication de l'article 
+        On limite le nombre de caractères du contenu affichés à l'accueil 
+        Et enfin, on ajoute un bouton qui redirige vers le billet en question -->
+
+        <div class="LastPost"><h4><?= (html_entity_decode($post['title'])) ?></h4></div>
+        <div class="trait"></div>
+        <p>Publié le <?= $post['added_datetime_fr'] ?></p>
+        <p><?= substr (nl2br(html_entity_decode($post ['content'])), 0, 350) . '...' ?>
+        <a href="?controller=PostController&action=showAction&id=<?= $post['id'] ?>" title="Lire le billet" >Lire la suite</a></p>
+        <br />
+    <?php
+}
+$posts->closeCursor();
 ?>
 
+<!-- On execute ob_get_clean qui lit le contenu courant du tampon de sortie puis l'efface -->
+
 <?php $content = ob_get_clean(); ?>
+
+<!-- On ajoute la vue de la template -->
 
 <?php require('views/template.php'); ?>
